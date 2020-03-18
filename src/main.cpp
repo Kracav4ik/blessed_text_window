96a28765bfe2948985d2d84fb29bc908560e0ad3
kracav4ik <dikama2013@yandex.ru>
2020-03-18 23:49:14 +0300

    SiMpLe MoUsE tEsT

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <curses.h>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    initscr();
+    noecho();
+    curs_set(0);
+    timeout(0);
+    keypad(stdscr, TRUE);
+    mouse_set(ALL_MOUSE_EVENTS);
+
+    std::string text = "Press q key to exit.";
+    while (true) {
+        mvaddstr(LINES / 2, (COLS - text.size()) / 2, text.c_str());
+
+        switch (getch()) {
+            case 'q':
+            case 'Q':
+                curs_set(1);
+                endwin();
+                return EXIT_SUCCESS;
+            case KEY_MOUSE:
+                request_mouse_pos();
+                mvaddch(MOUSE_Y_POS, MOUSE_X_POS, '*');
+                wprintw(stdscr, "Position: Y: %d X: %d", MOUSE_Y_POS, MOUSE_X_POS);
+                break;
+        }
+        napms(50);
+    }
 }
