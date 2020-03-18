5b6ca5b27a9de267becd33f1c9c6540b7fc093a6
kracav4ik <dikama2013@yandex.ru>
2020-03-19 02:44:18 +0300

    InPuT wItHoUt DeLaY

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
 #include <curses.h>
+#include <windows.h>
+#include <wincon/pdcwin.h>
+
+
+
+bool KEY_STATE[256] = {false};
+int MOUSE_X = 0;
+int MOUSE_Y = 0;
+DWORD MOUSE_STATE;
+
+
+void check_input() {
+    DWORD count;
+    GetNumberOfConsoleInputEvents(pdc_con_in, &count);
+
+    for (int i = 0; i < count; ++i) {
+        DWORD unused;
+        INPUT_RECORD ir;
+        ReadConsoleInput(pdc_con_in, &ir, 1, &unused);
+
+        if (ir.EventType == KEY_EVENT) {
+            const auto& e = ir.Event.KeyEvent;
+            KEY_STATE[e.wVirtualKeyCode] = e.bKeyDown;
+        } else if (ir.EventType == MOUSE_EVENT) {
+            const auto& e = ir.Event.MouseEvent;
+            MOUSE_X = e.dwMousePosition.X;
+            MOUSE_Y = e.dwMousePosition.Y;
+            MOUSE_STATE = e.dwButtonState;
+        }
+    }
+}
 
 int main() {
     // person coords
@@ -14,50 +45,53 @@ int main() {
     noecho();
     curs_set(0);
     timeout(0);
-    keypad(stdscr, TRUE);
-    mouse_set(ALL_MOUSE_EVENTS);
     bkgd(bg);
 
 
     while (true) {
         mvaddch(person_y, person_x, '@');
         auto move = [person_y, person_x, bg](int val, bool make_less, int clip) {
-            mvaddch(person_y, person_x, bg);
+//            mvaddch(person_y, person_x, bg);
             if (make_less) {
                 return (val + clip - 1) % clip;
             }
             return (val + 1) % clip;
         };
 
-        switch (getch()) {
-            case 'q': // exit
-            case 'Q':
-                curs_set(1);
-                endwin();
-                return EXIT_SUCCESS;
-            case KEY_MOUSE:
-                request_mouse_pos();
-                mvaddch(MOUSE_Y_POS, MOUSE_X_POS, '*');
-                wprintw(stdscr, "Position: Y: %d X: %d", MOUSE_Y_POS, MOUSE_X_POS);
-                break;
-            // moves
-            case 'w': // up
-            case 'W':
-                person_y = move(person_y, true, LINES);
-                break;
-            case 'a': // left
-            case 'A':
-                person_x = move(person_x, true, COLS);
-                break;
-            case 's': // down
-            case 'S':
-                person_y = move(person_y, false, LINES);
-                break;
-            case 'd': // right
-            case 'D':
-                person_x = move(person_x, false, COLS);
-                break;
+        check_input();
+
+        if ((MOUSE_STATE & FROM_LEFT_1ST_BUTTON_PRESSED) != 0) {
+            mvaddch(MOUSE_Y, MOUSE_X, '*');
+            wprintw(stdscr, "Position: Y: %d X: %d", MOUSE_Y, MOUSE_X);
         }
+
+        // exit
+        if (KEY_STATE['q'] || KEY_STATE['Q'] ) {
+            curs_set(1);
+            endwin();
+            return EXIT_SUCCESS;
+        }
+
+        // moves
+        // up
+        if (KEY_STATE['w'] || KEY_STATE['W'] ) {
+            person_y = move(person_y, true, LINES);
+        }
+        // left
+        if (KEY_STATE['a'] || KEY_STATE['A'] ) {
+
+            person_x = move(person_x, true, COLS);
+        }
+        // down
+        if (KEY_STATE['s'] || KEY_STATE['S'] ) {
+
+            person_y = move(person_y, false, LINES);
+        }
+        // right
+        if (KEY_STATE['d'] || KEY_STATE['D'] ) {
+            person_x = move(person_x, false, COLS);
+        }
+        getch();
         napms(50);
     }
 }
